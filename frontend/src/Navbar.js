afe4ab953f2e4f1fedc450e8eb5f37e355f0744f
OscarZurita <osczururp@alum.us.es>
2025-03-10 23:17:57 +0100

    docker config for mysql server

--- frontend/src/Navbar.js
+++ frontend/src/Navbar.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "./Navbar.css"; // Import styles
-import Dropdown from 'react-bootstrap/Dropdown';
 
 const Navbar = () => {
   return (
