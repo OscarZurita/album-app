5f653b16fdb22e371cb91986b8958f68a77ebc13
OscarZurita <osczururp@alum.us.es>
2025-03-13 23:19:25 +0100

    Dockerized spring application

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,26 +1,27 @@
+--this data.sql is intended to be run using the containerized application
 INSERT INTO photo (title, file_path, description, url, last_modified_date, upload_date)
-VALUES ('Laura con gorro', 'src/main/resources/Album1/Photo1.jpg', 'Laura con un gorro gracioso en el centro comercial de málaga', 'http://example.com/photo1', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+VALUES ('Laura con gorro', '/pictures/Album1/Photo1.jpg', 'Laura con un gorro gracioso en el centro comercial de málaga', 'http://example.com/photo1', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
 
 INSERT INTO photo (title, file_path, description, url, last_modified_date, upload_date)
-VALUES ('Photo2', 'src/main/resources/Album1/Photo2.jpg', 'Description for Photo2', 'http://example.com/photo2', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+VALUES ('Photo2', '/pictures/Album1/Photo2.jpg', 'Description for Photo2', 'http://example.com/photo2', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
 
 INSERT INTO photo (title, file_path, description, url, last_modified_date, upload_date)
-VALUES ('Photo3', 'src/main/resources/Album1/Photo3.jpg', 'Description for Photo3', 'http://example.com/photo3', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+VALUES ('Photo3', '/pictures/Album1/Photo3.jpg', 'Description for Photo3', 'http://example.com/photo3', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
 
 INSERT INTO photo (title, file_path, description, url, last_modified_date, upload_date)
-VALUES ('Photo4', 'src/main/resources/Album1/Photo4.jpg', 'Description for Photo4', 'http://example.com/photo4', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+VALUES ('Photo4', '/pictures/Album1/Photo4.jpg', 'Description for Photo4', 'http://example.com/photo4', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
 
 INSERT INTO photo (title, file_path, description, url, last_modified_date, upload_date)
-VALUES ('Photo5', 'src/main/resources/Album1/Photo5.jpg', 'Description for Photo5', 'http://example.com/photo5', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+VALUES ('Photo5', '/pictures/Album1/Photo5.jpg', 'Description for Photo5', 'http://example.com/photo5', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
 
 INSERT INTO photo (title, file_path, description, url, last_modified_date, upload_date)
-VALUES ('Photo6', 'src/main/resources/Album1/Photo6.jpg', 'Description for Photo6', 'http://example.com/photo6', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+VALUES ('Photo6', '/pictures/Album1/Photo6.jpg', 'Description for Photo6', 'http://example.com/photo6', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
 
 INSERT INTO photo (title, file_path, description, url, last_modified_date, upload_date)
-VALUES ('Photo7', 'src/main/resources/Album1/Photo7.jpg', 'Description for Photo7', 'http://example.com/photo7', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+VALUES ('Photo7', '/pictures/Album1/Photo7.jpg', 'Description for Photo7', 'http://example.com/photo7', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
 
 INSERT INTO photo (title, file_path, description, url, last_modified_date, upload_date)
-VALUES ('Photo8', 'src/main/resources/Album1/Photo8.jpg', 'Description for Photo8', 'http://example.com/photo8', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+VALUES ('Photo8', '/pictures/Album1/Photo8.jpg', 'Description for Photo8', 'http://example.com/photo8', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
 
 INSERT INTO photo (title, file_path, description, url, last_modified_date, upload_date)
-VALUES ('Photo9', 'src/main/resources/backgrounds/background1.jpg', 'background', 'http://example.com/background1', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+VALUES ('Photo9', '/pictures/backgrounds/background1.jpg', 'background', 'http://example.com/background1', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
